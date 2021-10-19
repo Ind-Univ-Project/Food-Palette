17f6456fa5ee6131228abad752ae6121aeefb20d
hhs629629 <hhs629629@gmail.com>
2021-10-19 19:00:38 +0900

    Image Analyzer 세부 구현 추가 구현, 에러핸들링, 로깅 추가

--- api_server/src/image_analyzer.rs
+++ api_server/src/image_analyzer.rs
@@ -1,7 +1,10 @@
 use crate::error::Error;
 use crate::pixel_data::PixelData;
 
-use image::{load_from_memory_with_format, DynamicImage, ImageFormat};
+use image::{load_from_memory_with_format, DynamicImage, GenericImageView, ImageFormat, Pixel};
+use uuid::Uuid;
+
+use async_std::fs;
 
 use tide::log::info;
 
@@ -14,17 +17,32 @@ impl ImageAnalyzer {
         info!("Creating Image Size: {}, Format: {}", buf.len(), format);
 
         let format = ImageFormat::from_extension(format).ok_or(Error::UnresolvableImageFormat)?;
-        let image = load_from_memory_with_format(buf, format)
-            .map_err(|e| Error::ImageLoadWithFormatError(e))?;
+        let image = load_from_memory_with_format(buf, format).map_err(|e| Error::ImageError(e))?;
 
         Ok(Self { image })
     }
 
     pub fn pixel_data(&self) -> PixelData {
-        todo!()
+        let mut result = PixelData::new(4, 4, 4);
+
+        for (_, _, pixel) in self.image.pixels() {
+            result.count_color(pixel.to_rgb());
+        }
+
+        result
     }
 
-    pub fn save_with_format(&self, format: ImageFormat) {
-        todo!()
+    pub async fn save_with_format(&self, format: ImageFormat) -> Result<(), Error> {
+        fs::create_dir_all("./data/images")
+            .await
+            .map_err(|e| Error::CreateDirectoryError(e))?;
+
+        let path = format!("./data/images/{}", Uuid::new_v4());
+
+        info!("Creating {}", path);
+        self.image.save_with_format(path, format)?;
+        info!("File created");
+        
+        Ok(())
     }
 }
