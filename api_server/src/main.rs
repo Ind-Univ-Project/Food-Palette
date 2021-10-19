58788e54450d691e879ad6e59ddfc64bf151a067
hhs629629 <hhs629629@gmail.com>
2021-10-19 17:41:11 +0900

    이미지 처리를 위한 적절한 모듈 추가

--- api_server/src/main.rs
+++ api_server/src/main.rs
@@ -1,8 +1,14 @@
 mod error;
+mod image_analyzer;
+mod pixel_data;
+mod rgb_ext;
+
+use error::Error;
+use image_analyzer::ImageAnalyzer;
 
 use tide::log::info;
 use tide::prelude::*;
-use tide::{Request, StatusCode};
+use tide::Request;
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
@@ -31,17 +37,14 @@ async fn get_image_list(req: Request<()>) -> tide::Result {
 }
 
 async fn upload_image(mut req: Request<()>) -> tide::Result {
-    let body = req.body_bytes().await.unwrap();
-    let image_type = req
-        .content_type()
-        .ok_or(tide::Error::from_str(
-            StatusCode::BadRequest,
-            "Getting Content-Type failed",
-        ))?
-        .subtype();
-    
-    //calculate each pixels
-    //save image to filesystem
-    //save data to database
+    info!(
+        "upload_image request Peer: {}",
+        req.peer_addr().ok_or(Error::PeerAddressError)?
+    );
+
+    //get body with multipart
+    //parse image category, image format, image data
+    //get pixel data
+
     todo!()
 }
