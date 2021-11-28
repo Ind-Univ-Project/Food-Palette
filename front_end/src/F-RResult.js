238cbc80474434ca87c050cbc5f03d197ecf4aa4
DA-EUN00 <ekdms3372766@naver.com>
2021-11-29 00:04:22 +0900

    Modified: 이미지 출력부분 수정

--- front_end/src/F-RResult.js
+++ front_end/src/F-RResult.js
@@ -7,7 +7,7 @@ function startLoadFile(){
     $.ajax({ 
         url: 'http://1.246.129.141/image/d9562bca-afe0-4f9a-8587-f17cd9b54e0f.png', 
         type: 'GET', 
-        dataType : 'binary', 
+        dataType : 'image/? ', 
         success : function (data) { 
             if(data) {
                 console.log('GET 완료!!');
@@ -23,8 +23,12 @@ function startLoadFile(){
 // JSON 포멧 데이터 처리 
 function createImages(objImageInfo) { 
     let image = objImageInfo;
+    let imageHTML = '';
+    const test = document.querySelector('div.gtr-50');
 
-
+    // GET으로 받아온 image 출력
+    imageHTMl += `<img src="${image}">`;
+    test.append(imageHTML);
 
     // var images = objImageInfo.images; 
     // var strDOM = ""; 
